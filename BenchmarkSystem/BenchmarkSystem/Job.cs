31dc9b4b769fa7b49150b146882367e8e247788d
Claus L. Henriksen <clih@itu.dk>
2012-09-25 14:38:16 +0200

    java doc

--- BenchmarkSystem/BenchmarkSystem/Job.cs
+++ BenchmarkSystem/BenchmarkSystem/Job.cs
@@ -5,6 +5,12 @@ using System.Text;
 
 namespace BenchmarkSystem {
   public class Job {
+    /// <summary>
+    /// Constructor for Job
+    /// </summary>
+    /// <param name="owner">Owner for class</param>
+    /// <param name="CPU">Number of threads</param>
+    /// <param name="ExpectedRuntime">Expected runtime</param>
     public Job(Owner owner, byte CPU, int ExpectedRuntime) {
       if (CPU > 7) throw new ArgumentOutOfRangeException("No more than 6 CPU's available. Tried to add job with: " + CPU);
       this.owner = owner;
@@ -12,43 +18,74 @@ namespace BenchmarkSystem {
       this.ExpectedRuntime = ExpectedRuntime;
       State = JobState.Created;
     }
+    /// <summary>
+    /// Owner of this job.
+    /// </summary>
     public Owner owner {
       get;
       set;
     }
 
+    /// <summary>
+    /// Number of threads used by this job
+    /// </summary>
     public byte CPU {
       get;
       set;
     }
 
+    /// <summary>
+    /// Expected runtime
+    /// </summary>
+    //TODO: Add unit
     public int ExpectedRuntime {
       get;
       set;
     }
 
+    /// <summary>
+    /// State of this job. See State enum
+    /// </summary>
     internal JobState State {
       get;
       set;
     }
 
+    /// <summary>
+    /// Algorithm this job runs
+    /// </summary>
     public Func<string[], string> process {
       get;
       set;
     }
 
+    /// <summary>
+    /// Timestamp. This should be set when the job is added to a queue. Use SetTimestamp()
+    /// </summary>
     public long timestamp {
       private set;
       public get;
     }
+
+    /// <summary>
+    /// Sets the timestamp of the job. This should be used when the job is added to the queue.
+    /// </summary>
     public void SetTimestamp() {
       timestamp = System.DateTime.Now.Millisecond;
     }
+
+    /// <summary>
+    /// Prints: Timestamp, owner, number of CPUs, expected runtime, state.
+    /// </summary>
+    /// <returns>String descriping this job</returns>
     public string ToString() {
       return "("+timestamp+")Job: [owner="+owner.Name+",CPU="+CPU+"ExpectedRuntime="+ExpectedRuntime+"] - "+State;
     }
   }
 
+  /// <summary>
+  /// Enum descriping the state of the Job.
+  /// </summary>
   enum JobState {
     Created,
     Queued,
