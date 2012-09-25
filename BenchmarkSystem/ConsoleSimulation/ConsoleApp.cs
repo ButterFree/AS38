211a26f1cd8c6b61a9c73652bd2bb959c52d60b1
Lasse Vork Borchert <monex_9@msn.com>
2012-09-25 15:58:36 +0200

    Updates

--- BenchmarkSystem/ConsoleSimulation/ConsoleApp.cs
+++ BenchmarkSystem/ConsoleSimulation/ConsoleApp.cs
@@ -11,16 +11,18 @@ namespace ConsoleSimulation
         static void Main(string[] args) {
             Owner me = new Owner("Test");
             BenchmarkSystemNs.BenchmarkSystem system = BenchmarkSystemNs.BenchmarkSystem.instance;
-            Logger logger = new Logger(system);
+            //Logger logger = new Logger(system);
             for (int i = 0; i < 100; i++) {
                 Job job = new Job(me, 2, 1);
                 job.process = (a) =>
                 {
-                    Console.WriteLine("Job" + i + " runs and runs");
+                    Console.WriteLine("");
                     return "";
                 };
                 system.Submit(job);
             }
+            
+            Console.WriteLine("Status: ");
             system.Status();
             Console.WriteLine("Executing all:");
             system.ExecuteAll();
